82ff3cbfffc344af9f070bbcc0a076647ee01b4d
Dan <dechever@caltech.edu>
2020-05-28 14:56:30 -0700

    Minor value updates to pupilBasedVFN simulations

--- main_pupilBasedVFN.m
+++ main_pupilBasedVFN.m
@@ -1,7 +1,7 @@
 clear; %close all; 
 addpath('VFNlib');
 
-N = 2^11; % Size of computational grid (NxN samples) 
+N = 2^12; % Size of computational grid (NxN samples) 
 apRad = 150; % Aperture radius in samples 
 charge = 1; % Charge of the vortex mask 
 
@@ -44,8 +44,8 @@ colormap(parula(256));
 
 %% Make vortex mask 
 
-offsetX = 0;%0.0952*apRad;
-offsetY = 0;%0.0524*apRad; 
+offsetX = 0;%0.095;%1.6147*apRad; % <-- PSF % 0.0952*apRad; % <-- best null position % 
+offsetY = 0;%0.052;%0.0011*apRad; % <-- PSF % 0.0524*apRad; % <-- best null position % 
 
 EPM = generateVortexMask( charge, coords, [offsetX offsetY] );
 
@@ -60,40 +60,40 @@ colorbar;
 %% Add Zernike aberration 
 
 noll_index = 7; % Noll index (Coma - vertical 90*)
-coeff = -0.0008; % waves rms
+coeff7 = -0.0006; %<--VortOut % -0.0008; %<--VortIn % % waves rms
 [Z,n,m] = generateZernike(noll_index,apRad,coords.RHO,coords.THETA);
 Z = Z/sqrt(mean(Z(logical(EP)).^2)); % Re-normalize (useful when pupil is not a circle)
-ABER = exp(1i*2*pi*coeff*Z);
+ABER = exp(1i*2*pi*coeff7*Z);
 
 noll_index = 8; % Noll index (Coma - horizontal 0*)
-coeff = 0.0033; % waves rms
+coeff8 = 0.0030; %<--VortOut % 0.0033; %<--VortIn % % waves rms
 [Z,n,m] = generateZernike(noll_index,apRad,coords.RHO,coords.THETA);
 Z = Z/sqrt(mean(Z(logical(EP)).^2)); % Re-normalize (useful when pupil is not a circle)
-ABER = ABER.*exp(1i*2*pi*coeff*Z);
+ABER = ABER.*exp(1i*2*pi*coeff8*Z);
 
 noll_index = 5; % Noll index (Astig - Oblique 45*) 
-coeff = -0.0099; % waves rms
+coeff5 =  -0.0104; %<--VortOut % -0.0099; %<--VortIn % % waves rms
 [Z,n,m] = generateZernike(noll_index,apRad,coords.RHO,coords.THETA);
 Z = Z/sqrt(mean(Z(logical(EP)).^2)); % Re-normalize (useful when pupil is not a circle)
-ABER = ABER.*exp(1i*2*pi*coeff*Z);
+ABER = ABER.*exp(1i*2*pi*coeff5*Z);
 
 noll_index = 6; % Noll index (Astig - Vertical 0*) 
-coeff = -0.0095; % waves rms
+coeff6 =  -0.0095; %<--VortOut % -0.0095; %<--VortIn % %waves rms
 [Z,n,m] = generateZernike(noll_index,apRad,coords.RHO,coords.THETA);
 Z = Z/sqrt(mean(Z(logical(EP)).^2)); % Re-normalize (useful when pupil is not a circle)
-ABER = ABER.*exp(1i*2*pi*coeff*Z);
+ABER = ABER.*exp(1i*2*pi*coeff6*Z);
 
 noll_index = 11; % Noll index (Primary Spherical) 
-coeff = 0.00; % waves rms
+coeff11 = 0;% -0.0194; % waves rms
 [Z,n,m] = generateZernike(noll_index,apRad,coords.RHO,coords.THETA);
 Z = Z/sqrt(mean(Z(logical(EP)).^2)); % Re-normalize (useful when pupil is not a circle)
-ABER = ABER.*exp(1i*2*pi*coeff*Z);
+ABER = ABER.*exp(1i*2*pi*coeff11*Z);
 
 noll_index = 4; % Noll index (Focus) 
-coeff = 0.01; % waves rms
+coeff4 = 0;%0.01; % waves rms
 [Z,n,m] = generateZernike(noll_index,apRad,coords.RHO,coords.THETA);
 Z = Z/sqrt(mean(Z(logical(EP)).^2)); % Re-normalize (useful when pupil is not a circle)
-ABER = ABER.*exp(1i*2*pi*coeff*Z);
+ABER = ABER.*exp(1i*2*pi*coeff4*Z);
 
 figure(3)
 imagesc(xvals/apRad,yvals/apRad,angle(EPM.*EP.*ABER));
@@ -117,23 +117,24 @@ colormap(parula(256));
 
 
 %% Coupling map
-
-fiberDiam = 1.45; % units of lambda/D
+% denominator = 1.4*lambda*F# <-- ideal Df for that F#
+fiberDiam = (4.64/4.64)*1.4; % units of lambda/D  
 
 % Parameters for Thorlabs SM600
     % link: https://www.thorlabs.com/NewGroupPage9_PF.cfm?ObjectGroup_ID=949
-fiber_props.type = 'bessel';
+fiber_props.type = 'gaussian';
 fiber_props.core_rad = 4.3e-6/2;% Core radius [um]
 lambda = 635e-9;% wavelength [um]
 fiber_props.n_core = 1.4606;% core index (interpolated from linear fit to 3 points)
 fiber_props.n_clad = 1.4571;% cladding index (interpolated from linear fit to 3 points)
-Fnum = 5; % optimal focal ratio
+Fnum = 5.22; % optimal focal ratio
 
-fibermode0 = generateSMFmode( fiber_props, lambda, Fnum, lambdaOverD, coords);
+%fibermode0 = generateSMFmode( n_core, n_clad, core_rad, lambda, lambda*Fnum/lambdaOverD, coords );
+fibermode0 = generateSMFmode_gaussian(fiberDiam*lambdaOverD,coords);
+%fibermode0 = generateSMFmode( fiber_props, lambda, Fnum, lambdaOverD, coords);
 
-%fibermode0 = generateSMFmode_gaussian(fiberDiam*lambdaOverD,coords);
-
-coupling_eff_map = generateCouplingMap( fibermode0, PSFv, totalPower0, 3*lambdaOverD, coords);
+abs(sum(sum(fibermode0.*PSFv))).^2/totalPower0
+%coupling_eff_map = generateCouplingMap( fibermode0, PSFv, totalPower0, 10*lambdaOverD, coords);
 
 figure(5)
 imagesc(xvals/lambdaOverD,yvals/lambdaOverD,coupling_eff_map);
@@ -142,6 +143,40 @@ axis([-3 3 -3 3]);
 title('Coupling map');
 colorbar; 
 colormap(parula(256));
+
+%% Save fits of coupling map
+% % Import library for managing fits headers
+% import matlab.io.*
+% % Create filename with path
+% fldnm = 'C:\Users\danie\OneDrive - California Institute of Technology\Mawet201718\VortexFiberNuller_VFN\Presentations\MonoWrapUp\NewAnalysis\';
+% flnm  = sprintf('Sim_Gauss_PSF_DF%0.2f_FN%0.2f_WFEOn.fits', fiberDiam, Fnum);
+% % Create fits
+% fitmap= fits.createFile([fldnm flnm]);
+% fits.createImg(fitmap,'double',size(coupling_eff_map));
+% %header data
+% fits.writeKey(fitmap, 'apRad   ', apRad);
+% fits.writeKey(fitmap, 'VortX   ', offsetX/apRad);
+% fits.writeKey(fitmap, 'VortY   ', offsetY/apRad);
+% fits.writeKey(fitmap, 'Noll4WF ', coeff4);
+% fits.writeKey(fitmap, 'Noll5WF ', coeff5);
+% fits.writeKey(fitmap, 'Noll6WF ', coeff6);
+% fits.writeKey(fitmap, 'Noll7WF ', coeff7);
+% fits.writeKey(fitmap, 'Noll8WF ', coeff8);
+% fits.writeKey(fitmap, 'Noll11WF', coeff11);
+% fits.writeKey(fitmap, 'fibDiam ', fiberDiam);
+% fits.writeKey(fitmap, 'Fnumber ', Fnum);
+% % Save
+% fits.setCompressionType(fitmap,'NOCOMPRESS');
+% fits.writeImg(fitmap,coupling_eff_map);
+% fits.closeFile(fitmap);
+
+%% Get Tip/Tilt sensitivity 
+% Optional section to calculate null depth given a fiber misplacement
+% val = 9e-9/(lambda*Fnum);
+% tilt = exp(1i*2*pi*val*lambdaOverD*coords.X/N);
+% FPp_t = myfft2(EP.*EPM.*tilt);
+% 
+% coup = abs(sum(sum(fibermode0.*FPp_t))).^2/totalPower0
 
 %% Get throughput curve for different F numbers 
 
