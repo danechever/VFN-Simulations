fa67d4b8ea666399150db765822c922cd7b04416
Dan <dechever@caltech.edu>
2022-05-10 12:22:15 -0700

    Fix minor bug in DEMO script for CRED2 Im Sim

--- KPICVFN_TrackCam_Simulator/DEMO_KPIC_image_generator.m
+++ KPICVFN_TrackCam_Simulator/DEMO_KPIC_image_generator.m
@@ -6,15 +6,15 @@ Npup = 2^7;     % pupil-plane sample grid size
 Nim  = 64;      % focal-plane sample grid size
 charge = 0;     % vortex charge to simulate
 lam0 = 1640e-9; % central wavelength 
-starHmag = 5.26;% H-mag of the star under consideration
+starHmag = 4.26;% H-mag of the star under consideration
 % Create struct
 cred2sim = setUp_CRED2ImSim(Npup, Nim, charge, lam0, starHmag);
 
 
 %-- Run simulation for 9 frames with no WFE but unique TT error
-Tint = 0.003;
-nolls = [5,8,9];
-coeffs = 0*[-0.08, 0.05, 0.09];
+Tint = 0.028;
+nolls = [5,8,9, 50:150];
+coeffs = [-0.08, 0.05, 0.09, 0.04*rand(1,length(nolls)-3)];
 nframes = 9;
 % Sample tilts from 2 gaussian distributions with different parameters
 sig_x = 10;     % [mas] rms X-jitter 
@@ -23,7 +23,7 @@ mu_x  = 0;      % [mas] average X position
 mu_y  = 0;      % [mas] average Y position
 tilts = [normrnd(mu_x, sig_x, [nframes,1]), normrnd(mu_y,sig_y,[nframes,1])];
 % Generate images
-[noisy, psf] = CRED2ImSim_getNoisyIm(cred2sim, Tint, nolls, coeffs, tilts);
+[noisy, psf] = CRED2ImSim_getNoisyIm(cred2sim, Tint, nolls, coeffs, tilts, true);
 
 %-- Display results
 figure();
