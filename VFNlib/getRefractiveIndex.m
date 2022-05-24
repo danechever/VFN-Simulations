2fc122ab85c6b2b348ce06ff27d77c4bfe3bb107
Dan <dechever@caltech.edu>
2022-05-24 11:04:24 -0700

    Fix some bugs in Refractive Index Database
    CaF2 had incorrect coefficients. Also modified code to take in wavelength in meters to match other functions in VFNlib. Updated source citations for future reference. Added warning for when out-of-bound wavelengths are supplied.

--- VFNlib/getRefractiveIndex.m
+++ VFNlib/getRefractiveIndex.m
@@ -4,56 +4,76 @@ function n = getRefractiveIndex(material,lambda)
 %
 %   Inputs:
 %       material - String indicating which material (e.g. 'CaF2')
-%       lambda - Wavelength in microns
+%       lambda - Wavelength in METERS
 %       
 %   Outputs:
 %       n - (Real) refractive index
 
-    if(material == 'NBK7')
-        %N-BK7 Sellmeier coefficients from Thorlabs
-        %Originally from
-        %citation
-        B0 = 0;
-        C1 = 0.00600069867;
-        B1 = 1.03961212;
-        C2 = 0.0200179144;
-        B2 = 0.231792344;
-        C3 = 103.560653;
-        B3 = 1.01046945;
+    lambda = lambda * 1e6;
+    
+    switch lower(material)
         
-    elseif(material == 'CaF2')
-        
-        B0 = 0.33973;
-        C1 = 0.09374;
-        B1 = 0.69913;
-        C2 = 21.18;
-        B2 = 0.11994;
-        C3 = 38.46^2;
-        B3 = 4.35181;
-        
-    elseif(material == 'BaF2')
-        B0 = 0.33973;
-        B1 = 0.81070;
-        C1 = 0.10065^2;
-        B2 = 0.19652;
-        C2 = 29.87^2;
-        B3 = 4.52469;
-        C3 = 53.82^2;
-        
-    elseif(material == 'ZnSe')
-        B0 = 0;
-        B1 = 4.45813734;
-        C1 = 0.200859853^2;
-        B2 = 0.467216334;
-        C2 = 0.391371166^2;
-        B3 = 2.8956629;
-        C3 = 47.1362108^2;
-        
-    else
-        error([material,'not yet implemented.']);
+        case 'nbk7' %'NBK7'
+            %N-BK7 Sellmeier coefficients from Thorlabs
+            % Matches this source as well: https://refractiveindex.info/?shelf=glass&book=BK7&page=SCHOTT
+            B0 = 0;
+            C1 = 0.00600069867;
+            B1 = 1.03961212;
+            C2 = 0.0200179144;
+            B2 = 0.231792344;
+            C3 = 103.560653;
+            B3 = 1.01046945;
+            
+            if (lambda < 0.3) || (lambda > 2.5)
+                warning('NBK7 coefficients only valid between 0.3-2.5um; update them in code if you want more wavelengths')
+            end
+
+        case 'caf2' %'CaF2'
+            % Source: https://refractiveindex.info/?shelf=main&book=CaF2&page=Li
+            B0 = 0.33973;
+            C1 = 0.09374^2;
+            B1 = 0.69913;
+            C2 = 21.18^2;
+            B2 = 0.11994;
+            C3 = 38.46^2;
+            B3 = 4.35181;
+            
+            if (lambda < 0.15) || (lambda > 12)
+                warning('CaF2 coefficients only valid between 0.15-12um; update them in code if you want more wavelengths')
+            end
+
+        case 'baf2' %'BaF2'
+            % Source: https://refractiveindex.info/?shelf=main&book=BaF2&page=Li
+            B0 = 0.33973;
+            B1 = 0.81070;
+            C1 = 0.10065^2;
+            B2 = 0.19652;
+            C2 = 29.87^2;
+            B3 = 4.52469;
+            C3 = 53.82^2;
+            
+            if (lambda < 0.15) || (lambda > 15)
+                warning('BaF2 coefficients only valid between 0.15-15um; update them in code if you want more wavelengths')
+            end
+
+        case 'znse' %'ZnSe'
+            % Source: https://refractiveindex.info/?shelf=main&book=ZnSe&page=Connolly
+            B0 = 0;
+            B1 = 4.45813734;
+            C1 = 0.200859853^2;
+            B2 = 0.467216334;
+            C2 = 0.391371166^2;
+            B3 = 2.8956629;
+            C3 = 47.1362108^2;
+
+            if (lambda < 0.54) || (lambda > 18.2)
+                warning('ZnSe coefficients only valid between 0.54-18.2um; update them in code if you want more wavelengths')
+            end
+
+        otherwise
+            error([material,'not yet implemented.']);
     end
     
     n = sqrt(1+B0+B1*lambda.^2./(lambda.^2-C1) + ...
                     B2*lambda.^2./(lambda.^2-C2) + ...
                     B3.*lambda.^2./(lambda.^2-C3));
-        
